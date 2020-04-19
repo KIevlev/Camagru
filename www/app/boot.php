cc826bd710137f8e0a7d99bde1f9ac05ba5e8f8d
KIevlev <Kika.Ievlev@yandex.ru>
2020-04-19 21:14:42 +0300

    Поудалял разного

--- www/app/boot.php
+++ www/app/boot.php
@@ -4,7 +4,5 @@ require_once 'core/model.php';
 require_once 'core/view.php';
 require_once 'core/controller.php';
 require_once 'core/route.php';
-//echo "Норм<br />";
 
-//echo "req - ".$_GET['request'];
 Route::start(); // запускаем маршрутизатор
